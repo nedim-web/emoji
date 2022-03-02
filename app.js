344dda8cb2054bd59ffbaad2526e9dfb962630b4
nedim-web <93007837+nedim-web@users.noreply.github.com>
2022-03-02 21:45:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const button = document.querySelector('input');
 const output = document.querySelector('.show-emoji');
 
 const htmlTemp = `
-    <img src="/emoji.png" alt="">
+    <img src="emoji.png" alt="">
 `;
 button.addEventListener('click', () => {
     console.log('op');
