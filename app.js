49dc49e9774bcb459f93cd32ac0a3f6b6a613071
nedim-web <93007837+nedim-web@users.noreply.github.com>
2022-03-02 21:43:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('input');
+const output = document.querySelector('.show-emoji');
+
+const htmlTemp = `
+    <img src="/emoji.png" alt="">
+`;
+button.addEventListener('click', () => {
+    console.log('op');
+    output.innerHTML += htmlTemp;
+});
